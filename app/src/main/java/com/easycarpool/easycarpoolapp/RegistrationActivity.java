7688b43aaf70bf9a15a81bc971a65872732fdcb3
ranjitjitu <ranjitjitu@gmail.com>
2016-06-08 01:14:31 +0530

    registration page changes

--- app/src/main/java/com/easycarpool/easycarpoolapp/RegistrationActivity.java
+++ app/src/main/java/com/easycarpool/easycarpoolapp/RegistrationActivity.java
@@ -2,6 +2,9 @@ package com.easycarpool.easycarpoolapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -9,6 +12,10 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        String [] compArray = getResources().getStringArray(R.array.company_arrays);
+        Spinner companySpinner = (Spinner)findViewById(R.id.company_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, compArray);
+        companySpinner.setAdapter(adapter);
         //comment
     }
 }
