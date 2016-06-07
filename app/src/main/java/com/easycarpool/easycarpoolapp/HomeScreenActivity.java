5069af36c18499dc56d5c3e363e47ceb785d3c88
ranjit <ranjitjitu@gmail.com>
2016-06-07 23:28:25 +0530

    Initial commit

---
+++ app/src/main/java/com/easycarpool/easycarpoolapp/HomeScreenActivity.java
@@ -0,0 +1,23 @@
+package com.easycarpool.easycarpoolapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getBaseContext(),RegistrationActivity.class);
+                startActivity(i);
+            }
+        }, 2000);
+    }
+}
